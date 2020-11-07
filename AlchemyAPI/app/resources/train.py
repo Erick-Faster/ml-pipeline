e65a5343974c2d192d4b61cd3834e2d2c1134ac3
Erick-Faster <erick.fasterra@gmail.com>
2020-11-06 21:53:19 -0300

    Link containers

--- AlchemyAPI/app/resources/train.py
+++ AlchemyAPI/app/resources/train.py
@@ -6,7 +6,7 @@ import requests
 class Train(Resource):
 
     def get(self):
-        url = 'http://127.0.0.1:6000/pipeline'
+        url = 'http://ml:6000/pipeline'
         headers = {"Content-Type": "application/json"}
         response_json = requests.get(url, headers=headers)
         response = json.loads(response_json.content)
